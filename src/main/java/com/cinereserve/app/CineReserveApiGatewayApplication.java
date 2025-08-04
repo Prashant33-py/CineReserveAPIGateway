9be4b74c25ab8ed5e8f9a20ab1c0a2884d2c2208
Prashant33-py <pvasala786@gmail.com>
2025-08-05 00:50:07 +0530

    first commit

---
+++ src/main/java/com/cinereserve/app/CineReserveApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.cinereserve.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineReserveApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineReserveApiGatewayApplication.class, args);
+	}
+
+}
